42b88a8fce5ae46b540767f3725c26d8a3ac69d4
Thomas Fritz <thomas.fritz.2000@gmx.de>
2023-01-07 18:06:10 +0100

    Made the exporter script usable for my docker setup
    ... while also changing variable names to make them easier to understand

--- jf_playlist_exporter.py
+++ jf_playlist_exporter.py
@@ -1,36 +1,36 @@
 import os
 import locale
+from glob import glob
 from xml.dom import minidom
 
 # Playlist directory.
-g_dir = "/var/lib/jellyfin/data/playlists"
-
+input_xml_playlist_directory = input("Input path to Jellyfin's XML playlist directory:")
+output_m3u8_playlist_directory = input("Input path to your output directory for the .m3u playlist files:")
 
 # There are subfolders in the playlist directory.
-for filename in os.listdir(g_dir):
-    g_subdir = os.path.join(g_dir, filename)
-    print(g_subdir)
+for playlist_name in os.listdir(input_xml_playlist_directory):
+    xml_playlist_subdir = os.path.join(input_xml_playlist_directory, playlist_name)
 
     # In the subfolders there are xml files
-    for xml_playlist in os.listdir(g_subdir):
-        g_fullpath = os.path.join(g_subdir, xml_playlist)
-        playlist_name = filename + '.m3u8'
-        f_path = os.path.join(g_dir,playlist_name)
-        print("Creating playlist")
-        #print(playlist_name)
+    for xml_playlist in glob(os.path.join(xml_playlist_subdir, "*.xml")):
+        xml_playlist_fullpath = os.path.join(xml_playlist_subdir, xml_playlist)
+        m3u8_playlist_name = playlist_name + '.m3u8'
+        m3u8_playlist_path = os.path.join(output_m3u8_playlist_directory, m3u8_playlist_name)
+        print("Creating playlist: " + playlist_name)
 
         # Start to write a file
-        with open(f_path, 'w') as writer:
+        with open(m3u8_playlist_path, 'w') as writer:
             writer.write("#EXTM3U")
             writer.write("\n")
 
             # Parse for each song in the playlist...
-            mydoc = minidom.parse(g_fullpath)
+            mydoc = minidom.parse(xml_playlist_fullpath)
             items = mydoc.getElementsByTagName('Path')
-        
+
             # add it to the file
             for elem in items:
-                writer.write(elem.firstChild.data.encode('utf8'))
+                writer.write(elem.firstChild.data)
                 writer.write("\n")
-        
+
 print("Done!")
+
